2976fe56670db3c08a3a118124b74b4a09e7927f
Ella Grace Henry <elhe2720@colorado.edu>
2026-04-02 10:59:50 -0600

    fitting lambda models with starting values from glm

--- scripts/SIR_model/main.R
+++ scripts/SIR_model/main.R
@@ -7,7 +7,9 @@ library(purrr)
 library(dplyr)
 library(ggplot2)
 library(MASS) 
-
+library(readxl)
+library(lme4)
+library(glmmLasso)
 
 source("scripts/ABM/R/activation_function.R")
 
@@ -36,3 +38,129 @@ colnames(colony) <- c("individual_ID","state","x","y")
 #compute cascade size across many simulations of group size and dose threshold
 
 #see how these cascade size distributions compare to real data
+
+
+#Fit a model for initator responder pairs
+setwd("~/Library/CloudStorage/GoogleDrive-elhe2720@colorado.edu/Shared drives/Field Research Videos/Gil Lab/Raw_Data/Curacao_2024/garden_eels/position_drop_experiment")
+data <- read_excel("master_ball_drop_data_3D_0216.xlsx")
+
+data$distance_to_ball <- sqrt((data$base_X - data$ball_hit_X)^2 + (data$base_Y - data$ball_hit_Y)^2 + (data$base_Z - data$ball_hit_Z)^2)
+
+#Summarise data by initiator-responder pairs
+initator_responder <- data %>%
+  group_by(drop_ID) %>%
+  mutate(responses = sum(full_partial_none > 0, na.rm=TRUE)) %>%
+  filter(responses > 0 & !is.na(full_partial_none)) %>%
+  filter(!trial_ID == 11) %>%
+  mutate(first_index = if (any(full_partial_none != 0 & !is.na(full_partial_none)))
+    which.min(ifelse(full_partial_none != 0 & !is.na(full_partial_none),
+                     response_frame_cam1, Inf))
+    else NA_integer_) %>%
+  mutate(
+    rank_order = rank(response_frame_cam1, na.last = "keep", ties.method = "first"),
+    initator = as.integer(rank_order == 1),
+    second_responder = as.integer(rank_order == 2),
+    first_x = base_X[first_index],
+    first_y = base_Y[first_index],
+    first_z = base_Z[first_index],
+    # Compute distance only for responders
+    dist_from_first_resp = ifelse(
+    !is.na(full_partial_none),
+    sqrt((base_X - first_x)^2 + (base_Y - first_y)^2 + (base_Z - first_z)^2),
+    NA_real_
+    )
+  ) %>%
+  filter(!is.na(dist_from_first_resp) & dist_from_first_resp > 0)%>%
+  ungroup()
+
+initator_responder$colony_eel_ID <- paste(initator_responder$colony, initator_responder$eel_ID, sep = "_")
+
+initator_responder <- initator_responder[
+  complete.cases(initator_responder[,c(
+    "second_responder",
+    "dist_from_first_resp",
+    "trial_ID",
+    "distance_to_ball"
+  )]),
+]
+
+initator_responder$trial_ID <- as.factor(initator_responder$trial_ID)
+
+initator_responder$colony <- as.factor(initator_responder$colony)
+
+initator_responder$colony_eel_ID <- as.factor(initator_responder$colony_eel_ID)
+
+# testing random effects structure
+intercepts_random_model <- glmer(second_responder ~ 1 + (1 | colony) + (1 | colony_eel_ID), family = binomial, data = initator_responder)
+summary(intercepts_random_model)
+
+#extract intercept
+beta_start <- as.numeric(fixef(intercepts_random_model)) 
+#Extract variance
+vc <- VarCorr(intercepts_random_model)
+var_colony     <- as.numeric(attr(vc[["colony"]],     "stddev")^2)
+var_colony_eel   <- as.numeric(attr(vc[["colony_eel_ID"]],"stddev")^2)
+var_start <- c(var_colony, var_colony_eel)
+
+predictors <- c("dist_from_first_resp", "distance_to_ball")
+start_vec <- c(beta_start, rep(0, length(predictors)), var_start)
+# Length should be: 1 (intercept) + 2 (fixed slopes) + 2 (variances) = 5
+cat("start_vec length:", length(start_vec), "\n")
+cat("start_vec:", start_vec, "\n")
+
+#Scan over a range of lambdas
+lambda_grid <- seq(1,3,by=1)
+
+#Fit model for lambda
+models <- lapply(lambda_grid, function(lambda) {
+       glmmLasso(
+             fix = second_responder ~ dist_from_first_resp + distance_to_ball,  # full model formula
+             rnd = list(colony = ~1, colony_eel_ID = ~1),
+             family = binomial(),
+             data = initator_responder,
+             lambda = lambda,
+             control = list(start = start_vec, center =TRUE)
+         )
+   })
+
+
+#test model
+test_model <- glmmLasso(
+       fix = second_responder ~ dist_from_first_resp + distance_to_ball,
+       rnd = list(colony = ~1, colony_eel_ID = ~1),
+       family = binomial(),
+       data = initator_responder,
+       lambda = 2
+       # no control/start - uses defaults
+     )
+
+summary(test_model)
+
+#create start vec
+start_vec = c(-1.46791, -0.70054, -0.16184, 0.3876931, 0.509688)
+
+#Scan over a range of lambdas
+lambda_grid <- seq(0.5,10,by=0.5)
+
+#Fit model for lambda
+models <- lapply(lambda_grid, function(lambda) {
+  glmmLasso(
+    fix = second_responder ~ dist_from_first_resp + distance_to_ball,  # full model formula
+    rnd = list(colony = ~1, colony_eel_ID = ~1),
+    family = binomial(),
+    data = initator_responder,
+    lambda = lambda,
+    control = list(start = start_vec, center =TRUE)
+  )
+})
+
+
+
+
+glmmLasso(fix = second_responder ~ dist_from_first_resp, data = initator_responder_clean, lambda = 1, family = binomial())
+
+#extract deviance:
+deviances <- sapply(models, function(m) {m$deviance})
+
+best_lambda <- lambda_grid[which.min(deviances)]
+best_model <- models[[which.min(deviances)]]
