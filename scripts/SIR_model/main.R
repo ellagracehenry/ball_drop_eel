8a17c973752ca029a54c2118fe8b4d140cd2b6c3
Ella Grace Henry <elhe2720@colorado.edu>
2026-05-25 10:52:19 -0600

    social only model

--- scripts/SIR_model/main.R
+++ scripts/SIR_model/main.R
@@ -1137,11 +1137,288 @@ param_grids <- expand.grid(threshold = c(1,5), tm = c(10), tr = c(5), fractional
 param_list <- split(param_grids, seq(nrow(param_grids)))
 #Eventually when model is a function: results <- pmap(param_grids, run_model)
 
+
+# Social and private information model
+social_private_frame_recorder_list <- vector(mode="list", length = length(unique(data$drop_ID)))
+names(social_private_frame_recorder_list) <- unique(data$drop_ID)
+
+
+for (i in unique(data$drop_ID)) {
+  
+  print(i)
+  
+  social_private_frame_recorder_list[[i]] <- vector(mode = "list", length = n_sims)
+  
+  #Calculate which individuals are emerged 
+  drop_data <- data %>%
+    filter(drop_ID == i & emerged == 1 & !is.na(global_X)) # & !is.na(dist_from_first_resp)
+  
+  drop_eel_IDs <- unique(drop_data$colony_eel_ID)
+  
+  if (fractional_contagion_first == TRUE) {
+    K_first <- length(drop_eel_IDs)
+  } else {
+    K_first <- 1
+  }
+  
+  if (length(drop_eel_IDs) < 3) next
+  
+  for (sim in 1:n_sims) {
+    
+    #create a frame recorder matrix
+    social_private_frame_recorder_matrix <- matrix(nrow=length(drop_eel_IDs), dimnames=list(drop_eel_IDs, NULL))
+    
+    resp_data <- as.data.frame(matrix(nrow=length(drop_eel_IDs),ncol=3))
+    
+    colony_idx <- which(unique(data$colony) == first(drop_data$colony))
+    
+    #time step 1
+    k <- 1
+    
+    #determine first responder
+    for (h in 1:length(drop_eel_IDs)) {
+      l_drop_ID <- first(drop_data$drop_ID)
+      l_colony_eel_ID <- drop_data$colony_eel_ID[h]
+      l_date <- first(drop_data$date)
+      l_colony <- first(drop_data$colony)
+      #for each eel i in drop j nested in colony k, compute the linear predictor
+      eta_j <- as.numeric(coefs[1]) + as.numeric(coefs[2])*(drop_data$log_distance_to_ball[h]) - ball_decay_time_coef*log(k) #RE removed for now... + fr_re_drop_ID$"(Intercept)"[fr_re_drop_ID$combo == l_drop_ID] + fr_re_colony_colony_eel_ID$"(Intercept)"[as.character(fr_re_colony_colony_eel_ID$name) == l_colony_eel_ID] + fr_re_date$"(Intercept)"[fr_re_date$combo == l_date] + fr_re_colony$"(Intercept)"[fr_re_colony$combo == l_colony]
+      #convert this to a standard logistic transform - gives probability per eel
+      p_private_cue <- 1/(1+exp(-eta_j))
+      resp_data[h,1] <- l_colony_eel_ID
+      resp_data[h,2] <- p_private_cue
+      resp_data[h,3] <- rbinom(n = 1, size = 1, prob = p_private_cue)
+      resp_data[h,4] <- resp_data[h,3]/K_first
+      resp_data[h,5] <- ifelse(resp_data[h,4] > private_threshold, 1, 0) #private threshold be on scale between 0 and 1
+      
+    }
+    
+    #if there is a first responder
+    if (sum(resp_data[,5], na.rm = TRUE) > 0) {
+      
+      #find IDs of first responder
+      fr_ID <- resp_data$V1[resp_data$V5 == 1]
+      
+      #find index of first responder
+      fr_idx <- which(drop_eel_IDs %in% fr_ID)
+      
+      social_private_frame_recorder_matrix[fr_idx] <- 1
+      
+      #create state matrix 
+      state_matrix <- matrix(nrow=length(drop_eel_IDs), ncol = 200)
+      state_matrix[fr_idx,1] <- "i"
+      state_matrix[-fr_idx,1] <- "s"
+      
+      #create dosage matrix 
+      dosage_matrix <- matrix(nrow=length(drop_eel_IDs), ncol = 200)
+      dosage_matrix[fr_idx,] <- NA
+      dosage_matrix[-fr_idx,] <- 0
+      
+      #for each time step 
+      for (k in 2:30) {
+        if (fractional_contagion_subs == TRUE) {
+          K <- sum(state_matrix[,k-1] == "s")
+        } else {
+          K <- 1
+        }
+        for (j in 1:length(drop_eel_IDs)) {
+          focal_eel_ID <- drop_eel_IDs[j]
+          
+          #Assigning states
+          if (state_matrix[j,k-1] == "r") { #if eel is recovered
+            state_matrix[j,k] <- "r"
+            dosage_matrix[j,k] <- NA
+          }
+          else if (state_matrix[j,k-1] == "i") { #if eel is infected
+            dosage_matrix[j,k] <- NA #state and frame recorder matrices stay the same
+            frames_since_infected <- k - social_private_frame_recorder_matrix[j]
+            if (k-tr <= 0) {
+              state_matrix[j,k] <- "i"
+              
+              #dose everyone
+              for (jj in 1:length(drop_eel_IDs)) {
+                buddy_eel_ID <- drop_eel_IDs[jj]
+                
+                if (buddy_eel_ID == focal_eel_ID) next  # skip self
+                
+                buddy_neighbours_ranked <- drop_data$inst_neighbours_topo_ranked[which(drop_data$colony_eel_ID == buddy_eel_ID)]
+                rank <- which(buddy_neighbours_ranked[[1]] == focal_eel_ID)
+                eta_j <- as.numeric(coefs[4])*log(rank) - social_decay_time_coef*log(frames_since_infected)
+                p_cue <- 1/(1+exp(-eta_j))
+
+                if (rbinom(1,1,p_cue*max_rate*dt) == 1) {
+                  dosage_matrix[jj,k] <- dosage_matrix[jj,k] + da
+                } else {
+                  
+                }
+              }
+            } else { 
+              if (state_matrix[j,k-tr] == "i") {
+                state_matrix[j,k] <- "r"
+              } else {
+                state_matrix[j,k] <- "i"
+                
+                #dose everyone
+                for (jj in 1:length(drop_eel_IDs)) {
+                  buddy_eel_ID <- drop_eel_IDs[jj]
+                  
+                  if (buddy_eel_ID == focal_eel_ID) next  # skip self
+                  
+                  buddy_neighbours_ranked <- drop_data$inst_neighbours_topo_ranked[which(drop_data$colony_eel_ID == buddy_eel_ID)]
+                  rank <- which(buddy_neighbours_ranked[[1]] == focal_eel_ID)
+                  eta_j <- as.numeric(coefs[4])*log(rank) - social_decay_time_coef*log(frames_since_infected)
+                  p_cue <- 1/(1+exp(-eta_j))
+                  
+                  if (rbinom(1,1,p_cue*max_rate*dt) == 1) {
+                    dosage_matrix[jj,k] <- dosage_matrix[jj,k] + da
+                  } else {
+                    
+                  }
+                }
+              }
+            }
+          } else { #eel is susceptible to hide
+            #Check if responds to private cue of the ball, just delayed
+            eta_j <-  as.numeric(coefs[1]) + as.numeric(coefs[2])*(drop_data$log_distance_to_ball[drop_data$colony_eel_ID == focal_eel_ID]) - ball_decay_time_coef*log(k) #Interecept (let's just fit it with the threshold) and RE removed for now... + fr_re_drop_ID$"(Intercept)"[fr_re_drop_ID$combo == l_drop_ID] + fr_re_colony_colony_eel_ID$"(Intercept)"[as.character(fr_re_colony_colony_eel_ID$name) == l_colony_eel_ID] + fr_re_date$"(Intercept)"[fr_re_date$combo == l_date] + fr_re_colony$"(Intercept)"[fr_re_colony$combo == l_colony]
+            #convert this to a standard logistic transform - gives probability per eel
+            p_private_cue <- 1/(1+exp(-eta_j))
+            private_cue_received <- rbinom(n = 1, size = 1, prob = p_private_cue) 
+            private_response <- ifelse(private_cue_received/K > private_threshold, 1, 0) #private threshold be on scale between 0 and 1
+            
+            
+            
+            #Check if responds to social cues
+            cuml_dose <- sum(dosage_matrix[j, 1:(k-1)], na.rm=TRUE)
+            norm_cuml_dose <- cuml_dose/K
+            
+            social_response <- ifelse(norm_cuml_dose > social_threshold, 1, 0)
+            
+            if (social_response == 1 | private_response == 1) {
+              state_matrix[j,k] <- "i"
+              social_private_frame_recorder_matrix[j] <- k
+              
+              #Dosing only starts next timestep
+              
+            } else {
+              state_matrix[j,k] <- "s"
+            }
+          }
+        }
+      }
+    }
+    social_private_frame_recorder_list[[i]][[sim]] <- social_private_frame_recorder_matrix
+  }
+}
+
+#Next step 
+# - make the non social model look like the social model - done
+# - make social only model thatthey only respond to social info after the first responder (first model is social + private
+# - make the 5 frame delay processing. First responders can respond in any of the first 5 frames. Subsequent responders only start responding from frame 5 and look to their dosage 5 frames previous from the current frame. 
+
+## Private-only model ##
+n_sims <- 10
+non_social_frame_recorder_list <- vector(mode="list", length = length(unique(data$drop_ID)))
+names(non_social_frame_recorder_list) <- unique(data$drop_ID)
+
+for (i in unique(data$drop_ID)) {
+  
+  print(i)
+  
+  non_social_frame_recorder_list[[i]] <- vector(mode = "list", length = n_sims)
+  
+  #Calculate which individuals are emerged 
+  drop_data <- data %>%
+    filter(drop_ID == i & emerged == 1 & !is.na(global_X)) 
+  
+  drop_eel_IDs <- unique(drop_data$colony_eel_ID)
+  
+  if (length(drop_eel_IDs) < 3) next
+  
+  for (sim in 1:n_sims) {
+    
+    #create a frame recorder matrix
+    non_social_frame_recorder_matrix <- matrix(nrow=length(drop_eel_IDs), dimnames=list(drop_eel_IDs, NULL))   
+    
+    resp_data <- as.data.frame(matrix(nrow=length(drop_eel_IDs),ncol=3))
+    
+    colony_idx <- which(unique(data$colony) == first(drop_data$colony))
+  
+    #determine first responder
+    for (h in 1:length(drop_eel_IDs)) {
+      l_drop_ID <- first(drop_data$drop_ID)
+      l_colony_eel_ID <- drop_data$colony_eel_ID[h]
+      l_date <- first(drop_data$date)
+      l_colony <- first(drop_data$colony)
+      #for each eel i in drop j nested in colony k, compute the linear predictor
+      eta_j <- as.numeric(coefs[1]) + as.numeric(coefs[2])*(drop_data$log_distance_to_ball[h])
+      #convert this to a standard logistic transform - gives probability per eel
+      p_private_cue <- 1/(1+exp(-eta_j))
+      resp_data[h,1] <- l_colony_eel_ID
+      resp_data[h,2] <- p_private_cue
+      resp_data[h,3] <- rbinom(n = 1, size = 1, prob = p_private_cue)
+      resp_data[h,4] <- resp_data[h,3]
+      resp_data[h,5] <- ifelse(resp_data[h,4] > private_threshold, 1, 0) #private threshold be on scale between 0 and 1
+    }
+    
+    #if there is a first responder
+    if (sum(resp_data[,5], na.rm = TRUE) > 0) {
+      
+      #find IDs of first responder
+      fr_ID <- resp_data$V1[resp_data$V5 == 1]
+      
+      #find index of first responder
+      fr_idx <- which(drop_eel_IDs %in% fr_ID)
+      
+      non_social_frame_recorder_matrix[fr_idx] <- 1
+      
+      #create state matrix 
+      state_matrix <- matrix(nrow=length(drop_eel_IDs), ncol = 200)
+      state_matrix[fr_idx,1] <- "i"
+      state_matrix[-fr_idx,1] <- "s"
+      
+      #for each time step
+      for (k in 2:30) {
+        for (j in 1:length(drop_eel_IDs)) {
+          focal_eel_ID <- drop_eel_IDs[j]
+          
+          if (state_matrix[j,k-1] == "r") { #if eel is recovered
+            state_matrix[j,k] <- "r"
+          } else if (state_matrix[j,k-1] == "i") { #if eel is infected
+            if (k-tr <= 0) {
+          state_matrix[j,k] <- "i"
+            } else {
+              if (state_matrix[j,k-tr] == "i") {
+                state_matrix[j,k] <- "r"
+              } else {
+                state_matrix[j,k] <- "i"
+              }
+            }
+          } else { #eel is susceptible to hide
+              #check if eel responds to private cue of the ball, just delayed
+            eta_j <- as.numeric(coefs[1]) + as.numeric(coefs[2])*(drop_data$log_distance_to_ball[drop_data$colony_eel_ID == focal_eel_ID]) - ball_decay_time_coef*log(k)
+            p_private_cue <- 1/(1+exp(-eta_j))
+            private_cue_received <- rbinom(n = 1, size = 1, prob = p_private_cue) 
+            private_response <- ifelse(private_cue_received/K > private_threshold, 1, 0) #private threshold be on scale between 0 and 1
+            
+            if (private_response == 1) {
+              state_matrix[j,k] <- "i"
+              non_social_frame_recorder_matrix[j] <- k
+            } else {
+              state_matrix[j,k] <- "s"
+            }
+            }
+            }
+      }
+    
+    non_social_frame_recorder_list[[i]][[sim]] <- non_social_frame_recorder_matrix
+    }
+  }
+}
+
+## Social-only model ##
 social_frame_recorder_list <- vector(mode="list", length = length(unique(data$drop_ID)))
 names(social_frame_recorder_list) <- unique(data$drop_ID)
 
-
-#2 - simulating the cascade at each drop 
 for (i in unique(data$drop_ID)) {
   
   print(i)
@@ -1244,7 +1521,7 @@ for (i in unique(data$drop_ID)) {
                 rank <- which(buddy_neighbours_ranked[[1]] == focal_eel_ID)
                 eta_j <- as.numeric(coefs[4])*log(rank) - social_decay_time_coef*log(frames_since_infected)
                 p_cue <- 1/(1+exp(-eta_j))
-
+                
                 if (rbinom(1,1,p_cue*max_rate*dt) == 1) {
                   dosage_matrix[jj,k] <- dosage_matrix[jj,k] + da
                 } else {
@@ -1277,22 +1554,14 @@ for (i in unique(data$drop_ID)) {
               }
             }
           } else { #eel is susceptible to hide
-            #Check if responds to private cue of the ball, just delayed
-            eta_j <-  as.numeric(coefs[1]) + as.numeric(coefs[2])*(drop_data$log_distance_to_ball[drop_data$colony_eel_ID == focal_eel_ID]) - ball_decay_time_coef*log(k) #Interecept (let's just fit it with the threshold) and RE removed for now... + fr_re_drop_ID$"(Intercept)"[fr_re_drop_ID$combo == l_drop_ID] + fr_re_colony_colony_eel_ID$"(Intercept)"[as.character(fr_re_colony_colony_eel_ID$name) == l_colony_eel_ID] + fr_re_date$"(Intercept)"[fr_re_date$combo == l_date] + fr_re_colony$"(Intercept)"[fr_re_colony$combo == l_colony]
-            #convert this to a standard logistic transform - gives probability per eel
-            p_private_cue <- 1/(1+exp(-eta_j))
-            private_cue_received <- rbinom(n = 1, size = 1, prob = p_private_cue) 
-            private_response <- ifelse(private_cue_received/K > private_threshold, 1, 0) #private threshold be on scale between 0 and 1
-            
-            
-            
+
             #Check if responds to social cues
             cuml_dose <- sum(dosage_matrix[j, 1:(k-1)], na.rm=TRUE)
             norm_cuml_dose <- cuml_dose/K
             
             social_response <- ifelse(norm_cuml_dose > social_threshold, 1, 0)
             
-            if (social_response == 1 | private_response == 1) {
+            if (social_response == 1) {
               state_matrix[j,k] <- "i"
               social_frame_recorder_matrix[j] <- k
               
@@ -1309,110 +1578,6 @@ for (i in unique(data$drop_ID)) {
   }
 }
 
-#Next step 
-# - make the non social model look like the social model
-# - make social only model thatthey only respond to social info after the first responder (first model is social + private
-# - make the 5 frame delay processing. First responders can respond in any of the first 5 frames. Subsequent responders only start responding from frame 5 and look to their dosage 5 frames previous from the current frame. 
-
-## Non social model ##
-n_sims <- 10
-non_social_frame_recorder_list <- vector(mode="list", length = length(unique(data$drop_ID)))
-names(non_social_frame_recorder_list) <- unique(data$drop_ID)
-
-for (i in unique(data$drop_ID)) {
-  
-  print(i)
-  
-  non_social_frame_recorder_list[[i]] <- vector(mode = "list", length = n_sims)
-  
-  #Calculate which individuals are emerged 
-  drop_data <- data %>%
-    filter(drop_ID == i & emerged == 1 & !is.na(global_X)) 
-  
-  drop_eel_IDs <- unique(drop_data$colony_eel_ID)
-  
-  if (length(drop_eel_IDs) < 3) next
-  
-  for (sim in 1:n_sims) {
-    
-    #create a frame recorder matrix
-    non_social_frame_recorder_matrix <- matrix(nrow=length(drop_eel_IDs), dimnames=list(drop_eel_IDs, NULL))   
-    
-    resp_data <- as.data.frame(matrix(nrow=length(drop_eel_IDs),ncol=3))
-    
-    colony_idx <- which(unique(data$colony) == first(drop_data$colony))
-  
-    #determine first responder
-    for (h in 1:length(drop_eel_IDs)) {
-      l_drop_ID <- first(drop_data$drop_ID)
-      l_colony_eel_ID <- drop_data$colony_eel_ID[h]
-      l_date <- first(drop_data$date)
-      l_colony <- first(drop_data$colony)
-      #for each eel i in drop j nested in colony k, compute the linear predictor
-      eta_j <- as.numeric(coefs[1]) + as.numeric(coefs[2])*(drop_data$log_distance_to_ball[h])
-      #convert this to a standard logistic transform - gives probability per eel
-      p_private_cue <- 1/(1+exp(-eta_j))
-      resp_data[h,1] <- l_colony_eel_ID
-      resp_data[h,2] <- p_private_cue
-      resp_data[h,3] <- rbinom(n = 1, size = 1, prob = p_private_cue)
-      resp_data[h,4] <- resp_data[h,3]
-      resp_data[h,5] <- ifelse(resp_data[h,4] > private_threshold, 1, 0) #private threshold be on scale between 0 and 1
-    }
-    
-    #if there is a first responder
-    if (sum(resp_data[,5], na.rm = TRUE) > 0) {
-      
-      #find IDs of first responder
-      fr_ID <- resp_data$V1[resp_data$V5 == 1]
-      
-      #find index of first responder
-      fr_idx <- which(drop_eel_IDs %in% fr_ID)
-      
-      non_social_frame_recorder_matrix[fr_idx] <- 1
-      
-      #create state matrix 
-      state_matrix <- matrix(nrow=length(drop_eel_IDs), ncol = 200)
-      state_matrix[fr_idx,1] <- "i"
-      state_matrix[-fr_idx,1] <- "s"
-      
-      #for each time step
-      for (k in 2:30) {
-        for (j in 1:length(drop_eel_IDs)) {
-          focal_eel_ID <- drop_eel_IDs[j]
-          
-          if (state_matrix[j,k-1] == "r") { #if eel is recovered
-            state_matrix[j,k] <- "r"
-          } else if (state_matrix[j,k-1] == "i") { #if eel is infected
-            if (k-tr <= 0) {
-          state_matrix[j,k] <- "i"
-            } else {
-              if (state_matrix[j,k-tr] == "i") {
-                state_matrix[j,k] <- "r"
-              } else {
-                state_matrix[j,k] <- "i"
-              }
-            }
-          } else { #eel is susceptible to hide
-              #check if eel responds to private cue of the ball, just delayed
-            eta_j <- as.numeric(coefs[1]) + as.numeric(coefs[2])*(drop_data$log_distance_to_ball[drop_data$colony_eel_ID == focal_eel_ID]) - ball_decay_time_coef*log(k)
-            p_private_cue <- 1/(1+exp(-eta_j))
-            private_cue_received <- rbinom(n = 1, size = 1, prob = p_private_cue) 
-            private_response <- ifelse(private_cue_received/K > private_threshold, 1, 0) #private threshold be on scale between 0 and 1
-            
-            if (private_response == 1) {
-              state_matrix[j,k] <- "i"
-              non_social_frame_recorder_matrix[j] <- k
-            } else {
-              state_matrix[j,k] <- "s"
-            }
-            }
-            }
-      }
-    
-    non_social_frame_recorder_list[[i]][[sim]] <- non_social_frame_recorder_matrix
-    }
-  }
-}
 
 #Response vs no confusion matrix 
 library(tidyverse)
